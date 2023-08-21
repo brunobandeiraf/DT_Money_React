f0308d2005a4b0edc9c2ecf573e0113451dfd7c0
Bruno Bandeira <brunobandeirafernandes@gmail.com>
2023-08-20 22:59:06 -0300

    criando hook de resumo

--- json-server/src/components/Summary/index.tsx
+++ json-server/src/components/Summary/index.tsx
@@ -3,36 +3,13 @@ import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
 import { SummaryCard, SummaryContainer } from './styles';
 import { priceFormatter } from '../../utils/formatter';
+import { useSummary } from '../../hooks/useSummary';
 
 export function Summary() {
 
-  // Contexto instanciado para ter acesso ao array de transações
-  const { transactions } = useContext(TransactionsContext);
+  
 
-  // .reduce percorre o array a alguma estrutura de dados
-  // { income: 0, outcome: 0, total: 0 }
-  // .reduce(() => {}, { income: 0, outcome: 0, total: 0 })
-  // 
-  const summary = transactions.reduce(
-    (acc, transaction) => { 
-    // acc é o estado inicial
-    // transaction é cada interação
-      if (transaction.type === 'income') {
-        acc.income += transaction.price;
-        acc.total += transaction.price;
-      } else {
-        acc.outcome += transaction.price;
-        acc.total -= transaction.price;
-      }
-
-      return acc;
-    },
-    {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    },
-  );
+  const summary = useSummary();
 
   return (
     <SummaryContainer>
---
+++ json-server/src/hooks/useSummary.ts
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+import { TransactionsContext } from '../contexts/TransactionsContext'
+
+export function useSummary() {
+
+    // Contexto instanciado para ter acesso ao array de transações
+    const { transactions } = useContext(TransactionsContext);
+
+    // .reduce percorre o array a alguma estrutura de dados
+    // { income: 0, outcome: 0, total: 0 }
+    // .reduce(() => {}, { income: 0, outcome: 0, total: 0 })
+    // 
+    const summary = transactions.reduce(
+        (acc, transaction) => { 
+        // acc é o estado inicial
+        // transaction é cada interação
+        if (transaction.type === 'income') {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+        } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+        }
+
+        return acc;
+        },
+        {
+        income: 0,
+        outcome: 0,
+        total: 0,
+        },
+    );
+
+    return summary;
+}
